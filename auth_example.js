6c3f446ccbfe2d0ef9072666ef191b7984220b25
ericmartens <ericmartens@gmail.com>
2020-04-26 17:04:10 -0600

    Example auth js file

---
+++ auth_example.js
@@ -0,0 +1,16 @@
+//this file contains service credentials for your IBM services. replace the variables below
+//with your credentials, then rename this file to auth.js prior to pushing to IBM Cloud
+module.exports = {
+    cloud: {
+        apikey: "xxxxxPASTE HERExxxxx"
+    },
+    wml: {
+        "apikey": "key",
+        "iam_apikey_description": "description",
+        "iam_apikey_name": "auto-generated-apikey",
+        "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Writer",
+        "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::",
+        "instance_id": "instance_id",
+        "url": "https://us-south.ml.cloud.ibm.com",
+    }
+};
